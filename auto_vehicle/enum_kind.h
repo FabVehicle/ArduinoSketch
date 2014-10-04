9e5527a3845cf513d0572cda67b05e1dbad6b923
黒川圭一 <kei.67.vagr@gmail.com>
2014-10-04 12:50:08 +0900

    [auto_vehicle] 30cmと15cmで反応するに変更

---
+++ auto_vehicle/enum_kind.h
@@ -0,0 +1,15 @@
+typedef enum VoiceKind_ {
+  vcSilence   = 0,
+  vcNearRight = 1,
+  vcNearLeft  = 2,
+  vcFarRight  = 3,
+  vcFarLeft   = 4,
+  vcStraight  = 5,
+  vcBack      = 6
+} VoiceKind;
+
+typedef enum DisKind_ {
+  disUnknown = 0,
+  disNear    = 1,
+  disFar     = 2
+} DisKind;
